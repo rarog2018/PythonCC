59a3b8d8f0f74583408a94e096e29f287ec5fdb0
rarog2018 <46136924+rarog2018@users.noreply.github.com>
2019-01-30 17:38:44 +0100

    Refactoring, added get_step() method

--- C15 Generating Data/random_walk.py
+++ C15 Generating Data/random_walk.py
@@ -12,19 +12,21 @@ class RandomWalk():
 		self.x_values = [0]
 		self.y_values = [0]
 		
+	def get_step(self):
+		"""Determine the direction and distance for each step"""
+		direction = choice([0.5, -0.5])
+		distance = choice([1, 8])
+		step = direction * distance
+		return step
+	
 	def fill_walk(self):
 		"""Calculate all the points in the walk"""
 		
 		#Keep taking steps until walk reaches the desired length
 		while len(self.x_values) < self.num_points:
 			#Decide which direction to go and how far to go in that direction
-			x_direction = choice([1, -1])
-			x_distance = choice([1, 2, 3, 4])
-			x_step = x_direction * x_distance
-			
-			y_direction = choice([1, -1])
-			y_distance = choice([1, 2, 3, 4])
-			y_step = y_direction * y_distance
+			x_step = self.get_step()
+			y_step = self.get_step()
 			
 			#Reject moves that go nowhere
 			if x_step == 0 and y_step == 0:
